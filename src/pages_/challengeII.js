e1457bb65f07c646953e0230b0c4f497203c0874
Grzegorz Telka <you@example.com>
2021-03-14 10:38:16 +0100

    new scoring

--- src/pages_/challengeII.js
+++ src/pages_/challengeII.js
@@ -109,50 +109,51 @@ const ChallengeII = () => {
                                             <tr>
                                                 <td>1.</td>
                                                 <td>Julia i Hubert</td>
-                                                <td>69</td>
-                                                <td>0</td>
+                                                <td>81</td>
+                                                <td>58</td>
                                                 <td>34</td>
 
                                             </tr>
                                             <tr>
                                                 <td>2.</td>
                                                 <td>MŁ</td>
-                                                <td>41</td>
+                                                <td>58</td>
                                                 <td>0</td>
-                                                <td>32</td>
+                                                <td>54</td>
 
 
                                             </tr>
                                             <tr>
                                                 <td>3.</td>
                                                 <td>Fire</td>
-                                                <td>24</td>
-                                                <td>0</td>
-                                                <td>4</td>
+                                                <td>31</td>
+                                                <td>1</td>
+                                                <td>5</td>
 
                                             </tr>
                                             <tr>
                                                 <td>4.</td>
-                                                <td>Pączki</td>
-                                                <td>20</td>
-                                                <td>0</td>
-                                                <td>7</td>
+                                                <td>Kwintet Fortepianowy I</td>
+                                                <td>23</td>
+                                                <td>36</td>
+                                                <td>5</td>
+
 
                                             </tr>
                                             <tr>
                                                 <td>5.</td>
-                                                <td>Kwintet Fortepianowy I</td>
-                                                <td>18</td>
-                                                <td>12</td>
-                                                <td>4</td>
+                                                <td>Pączki</td>
+                                                <td>20</td>
+                                                <td>8</td>
+                                                <td>7</td>
 
                                             </tr>
                                             <tr>
                                                 <td>6.</td>
                                                 <td>Wronki</td>
-                                                <td>7</td>
+                                                <td>11</td>
                                                 <td>0</td>
-                                                <td>13</td>
+                                                <td>20</td>
 
                                             </tr>
                                             <tr>
@@ -167,7 +168,7 @@ const ChallengeII = () => {
                                                 <td>8.</td>
                                                 <td>Zastrzyk Wiedzy</td>
                                                 <td>6</td>
-                                                <td>0</td>
+                                                <td>4</td>
                                                 <td>3</td>
 
                                             </tr>
@@ -175,16 +176,16 @@ const ChallengeII = () => {
                                                 <td>9.</td>
                                                 <td>Joanna Kubacka</td>
                                                 <td>6</td>
-                                                <td>0</td>
+                                                <td>2</td>
                                                 <td>3</td>
 
                                             </tr>
                                             <tr>
                                                 <td>10.</td>
-                                                <td>Klaudialb</td>
-                                                <td>2</td>
+                                                <td>Marsz do Nauki2</td>
+                                                <td>4</td>
+                                                <td>8</td>
                                                 <td>0</td>
-                                                <td>2</td>
 
                                             </tr>
 
