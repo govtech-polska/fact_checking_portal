edeca7eea96b60627ad7435382b0760b3e815e9d
Grzegorz Telka <you@example.com>
2021-03-19 12:37:33 +0100

    plansza z wynikami

--- src/pages_/challengeII.js
+++ src/pages_/challengeII.js
@@ -92,12 +92,9 @@ const ChallengeII = () => {
                         <div className="container-fluid">
                             <div className="row no-gutters">
                                 <div className="col-lg-6">
-                                    <div className="iframeWrapper">
-                                        <iframe
-                                            webkitAllowFullScree mozallowfullscreen allowFullScreen
-                                            src="//ljsp.lwcdn.com/api/video/embed.jsp?id=2b275b50-2fea-4968-b2b1-c7f3a77e3d18&pi=6ccbd9c0-a038-4243-b85b-cdfa19dd5c05"
-                                            title="0" byline="0" portrait="0" width="640" height="360"
-                                            frameBorder="0" allow="autoplay"></iframe>
+                                    <div className="imageWrapper">
+                                        <img src="/images/zwyciezcyIIedycji.png" alt=""/>
+
                                     </div>
 
                                 </div>
