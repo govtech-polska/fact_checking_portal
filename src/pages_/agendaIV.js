591160fd8e3543902431838a8ba943dd34b3daf1
tomaszkapuscinski <70897907+tomaszkapuscinski@users.noreply.github.com>
2021-11-17 09:55:26 +0100

    add person

--- src/pages_/agendaIV.js
+++ src/pages_/agendaIV.js
@@ -228,7 +228,7 @@ const AgendaIV = () => {
                                 <ul className="personList">
                                     <li id="id1" className="person">
                                         <div className="imageWrapper">
-                                            <img src="/images/persons/fh4/1.png"/>
+                                            <img src="/images/persons/fh4/1.jpg"/>
                                         </div>
                                         <div className="textWrapper">
                                             <h2 className="name">{t('agendaIV:personList.person1.name')}</h2>
