07274fdfb6ab6512d867c205dda283240fa31791
Tomasz Kapuściński <t.kapuscinski@pap.pl>
2021-12-03 09:21:11 +0100

    Add iframe

--- src/pages_/challengeIV.js
+++ src/pages_/challengeIV.js
@@ -94,12 +94,19 @@ const challengeIV = () => {
 
 
                                 <div className="col-lg-6">
-                                    <div className="imageWrapper">
+                                    <div className="iframeWrapper">
+                                        <iframe webkitAllowFullScreen mozallowfullscreen allowFullScreen
+                                                src="//ljsp.lwcdn.com/api/video/embed.jsp?id=2b275b50-2fea-4968-b2b1-c7f3a77e3d18&pi=6ccbd9c0-a038-4243-b85b-cdfa19dd5c05"
+
+                                                title="0" byline="0" portrait="0" frameBorder="0" allow="autoplay"
+                                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+                                    </div>
+                                    {/*<div className="imageWrapper">
 
                                         <img src="/images/micro.png" alt="eksperckie_spotkania"/>
 
 
-                                    </div>
+                                    </div>*/}
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="textWrapper">
