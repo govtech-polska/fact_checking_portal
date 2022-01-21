60b39974a5f962197d9d9883b3f39620f61bd148
tomaszkapuscinski <70897907+tomaszkapuscinski@users.noreply.github.com>
2022-01-21 07:33:12 +0100

    add person

--- src/pages_/zespol.js
+++ src/pages_/zespol.js
@@ -68,6 +68,11 @@ const Team = () => {
 
                 <ul>
                   <li>
+                    <strong>{t('team:data.18.name')}</strong>
+                    <p>{t('team:data.18.description')}</p>
+                  </li>
+
+                  <li>
                     <strong>{t('team:data.4.name')}</strong>
                     <p>{t('team:data.4.description')}</p>
                   </li>
