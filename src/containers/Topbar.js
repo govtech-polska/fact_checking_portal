1dda3c84846cbe6e3109bb8fdff422e61abd0f2e
tomaszkapuscinski <70897907+tomaszkapuscinski@users.noreply.github.com>
2023-04-05 11:25:28 +0200

    Update Topbar.js
    change link in menu

--- src/containers/Topbar.js
+++ src/containers/Topbar.js
@@ -249,8 +249,8 @@ const Topbar = () => {
             <li className="topbar-listItem"><a className="topbar-link" href="https://fake-hunter.pap.pl/dolacz-do-partnerow">Dołącz do partnerów</a>
             </li>
             <li className="topbar-listItem"><a className="topbar-link" href="https://fake-hunter.pap.pl/edukacja">#FakeHunter - Edu</a></li>
-            <li className="topbar-listItem"><a className="topbar-link" href="https://fake-hunter.pap.pl/challengeVI">#FakeHunter
-              Challenge - Wojna Informacyjna</a></li>
+            <li className="topbar-listItem"><a className="topbar-link" href="https://fake-hunter.pap.pl/challengeVII">#FakeHunter
+              Challenge - Fake News Alert</a></li>
           </ul>
 
         </Content>
