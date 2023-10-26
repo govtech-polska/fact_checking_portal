bb987fa5e8bace05391349c9c9e4b40ea7fd764d
tomaszkapuscinski <70897907+tomaszkapuscinski@users.noreply.github.com>
2023-10-26 12:21:57 +0200

    Revert "twitter logo"
    This reverts commit ec449c818499044103c5d4fd328093659a995c6d.

--- src/components/SocialShare.js
+++ src/components/SocialShare.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types'
 import {
-    FacebookShareButton,
-    FacebookIcon,
-    PocketShareButton,
-    TwitterShareButton,
-    WhatsappShareButton,
-    TwitterIcon,
-    PocketIcon,
-    WhatsappIcon
+  FacebookShareButton,
+  FacebookIcon,
+  PocketShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  TwitterIcon,
+  PocketIcon,
+  WhatsappIcon
 } from 'react-share'
 import styled from 'styled-components'
 
@@ -41,47 +41,45 @@ const Label = styled.p`
 `
 
 const WykopShareButton = createShareButton(
-    'wykop',
-    (url) => url,
-    () => ({}),
-    {
-        windowWidth: 660,
-        windowHeight: 650
-    }
+  'wykop',
+  (url) => url,
+  () => ({}),
+  {
+    windowWidth: 660,
+    windowHeight: 650
+  }
 )
 
-const SocialShare = ({url, title, description}) => {
-    const {t} = useTranslation()
-    return (
-        <Wrapper>
-            <Label>{t('verified:report.share')}</Label>
-            <FacebookShareButton url={url}>
-                <FacebookIcon size={32}/>
-            </FacebookShareButton>
-            <TwitterShareButton url={url}>
-                <img
-                    src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMEgzMlYzMkgwVjBaIiBmaWxsPSJibGFjayIvPgo8cGF0aCBkPSJNMTcuMzA5NCAxNC4wNDYxTDI0LjM4MzggNkgyMi43MDhMMTYuNTYyNyAxMi45ODQ5TDExLjY1ODIgNkg2TDEzLjQxODIgMTYuNTYzNEw2IDI1SDcuNjc1OEwxNC4xNjExIDE3LjYyMjFMMTkuMzQxOCAyNUgyNU04LjI4MDYzIDcuMjM2NUgxMC44NTUxTDIyLjcwNjcgMjMuODI0MkgyMC4xMzE2IiBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4K"
-                    alt="Twitter"/>
-            </TwitterShareButton>
-            <WhatsappShareButton url={url} windowWidth={660} windowHeight={700}>
-                <WhatsappIcon size={32} title="Wyślij na WhatsApp"/>
-            </WhatsappShareButton>
-            <PocketShareButton url={url}>
-                <PocketIcon size={32} title="Dodaj do Pocket"/>
-            </PocketShareButton>
-            <WykopShareButton
-                url={`http://www.wykop.pl/dodaj/link/?url=${url}&title=${title}&desc=${description}`}
-            >
-                <img alt="Wykop" src="/wykop-icon.png" title="Wykop"/>
-            </WykopShareButton>
-        </Wrapper>
-    )
+const SocialShare = ({ url, title, description }) => {
+  const { t } = useTranslation()
+  return (
+    <Wrapper>
+      <Label>{t('verified:report.share')}</Label>
+      <FacebookShareButton url={url}>
+        <FacebookIcon size={32} />
+      </FacebookShareButton>
+      <TwitterShareButton url={url}>
+        <TwitterIcon size={32} title="Udostępnij na Twitterze" />
+      </TwitterShareButton>
+      <WhatsappShareButton url={url} windowWidth={660} windowHeight={700}>
+        <WhatsappIcon size={32} title="Wyślij na WhatsApp" />
+      </WhatsappShareButton>
+      <PocketShareButton url={url}>
+        <PocketIcon size={32} title="Dodaj do Pocket" />
+      </PocketShareButton>
+      <WykopShareButton
+        url={`http://www.wykop.pl/dodaj/link/?url=${url}&title=${title}&desc=${description}`}
+      >
+        <img alt="Wykop" src="/wykop-icon.png" title="Wykop" />
+      </WykopShareButton>
+    </Wrapper>
+  )
 }
 
 SocialShare.propTypes = {
-    url: PropTypes.string,
-    title: PropTypes.string,
-    description: PropTypes.string
+  url: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string
 }
 
 export default SocialShare
